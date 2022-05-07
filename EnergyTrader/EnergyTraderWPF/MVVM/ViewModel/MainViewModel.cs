79ff2d44f5b2119284ec3a5b195d008b45ad9922
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-05-07 10:03:43 +0200

    Created a skeleton for the application

---
+++ EnergyTrader/EnergyTraderWPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using EnergyTraderWPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyTraderWPF.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand DashBoardViewCommand { get; set; }
+        public RelayCommand WindTurbinesViewCommand { get; set; }
+        public RelayCommand SolarPanelsViewCommand { get; set; }
+        public DashboardViewModel DashboardVM { get; set; }
+        public WindTurbinesViewModel WindTurbinesVM { get; set; }
+        public SolarPanelsViewModel SolarPanelsVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            DashboardVM = new DashboardViewModel();
+            WindTurbinesVM = new WindTurbinesViewModel();
+            SolarPanelsVM = new SolarPanelsViewModel();
+            CurrentView = DashboardVM;
+
+            DashBoardViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = DashboardVM;
+            });
+
+            WindTurbinesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = WindTurbinesVM;
+            });
+
+            SolarPanelsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SolarPanelsVM;
+            });
+
+        }
+    }
+}
